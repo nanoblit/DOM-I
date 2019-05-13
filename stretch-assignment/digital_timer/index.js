d74b3c346c8f80fdc4b53f747a9ccbdf58afb451
nanoblit <nanoblit@users.noreply.github.com>
2019-05-13 14:37:04 +0200

    Finish timer stretches

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,27 +1,47 @@
-const startTimer = () => {
-    let msTens = document.querySelector("#msTens");
-    let msHundreds = document.querySelector("#msHundreds");
-    let secondOnes = document.querySelector("#secondOnes");
-    let secondTens = document.querySelector("#secondTens");
-    let time = 0;
+let msTens = document.querySelector("#msTens");
+let msHundreds = document.querySelector("#msHundreds");
+let secondOnes = document.querySelector("#secondOnes");
+let secondTens = document.querySelector("#secondTens");
+let time = 0;
+let timeout = null;
 
-    const runTimer = () => {
-        time++;
+const startBtn = document.querySelector("#startBtn");
+const resetBtn = document.querySelector("#resetBtn");
 
-        msTens.textContent = Math.floor(time % 10);
-        msHundreds.textContent = Math.floor((time / 10) % 10);
-        secondOnes.textContent = Math.floor((time / 100) % 10);
-        secondTens.textContent = Math.floor((time / 1000) % 10);
+const runTimer = () => {
+    time++;
 
-        if (time >= 1000) {
-            document.querySelector(".digits").style.color = "red";
-            return;
-        }
+    msTens.textContent = Math.floor(time % 10);
+    msHundreds.textContent = Math.floor((time / 10) % 10);
+    secondOnes.textContent = Math.floor((time / 100) % 10);
+    secondTens.textContent = Math.floor((time / 1000) % 10);
 
-        setTimeout(runTimer, 10);
-    };
+    if (time >= 1000) {
+        document.querySelector(".digits").style.color = "red";
+        startBtn.disabled = false;
+        return;
+    }
 
-    runTimer();
+    timeout = setTimeout(runTimer, 10);
 };
 
-startTimer();
+const resetTimer = () => {
+    document.querySelector(".digits").style.color = "black";
+    time = 0;
+    msTens.textContent = "-";
+    msHundreds.textContent = "-";
+    secondOnes.textContent = "-";
+    secondTens.textContent = "-";
+    if (timeout) clearTimeout(timeout);
+}
+
+startBtn.addEventListener("click", e => {
+    e.target.disabled = true;
+    resetTimer();
+    runTimer();
+});
+
+resetBtn.addEventListener("click", e => {
+    resetTimer();
+    startBtn.disabled = false;
+});
